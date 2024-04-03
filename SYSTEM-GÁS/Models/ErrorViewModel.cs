09f3630ce6f48499bee64b817342156117fa9422
Juan <juanstdias@gmail.com>
2024-04-02 21:13:42 -0300

    Adicionar arquivos de projeto.

---
+++ SYSTEM-GÁS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SYSTEM_GÁS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
